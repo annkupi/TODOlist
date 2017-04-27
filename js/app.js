35171ecef4ca4c2113229f82b25edde9c5905ef9
annkupi <anna-kupina@yandex.ru>
2017-04-27 19:46:00 +0300

    Big changes -list print -add new task

--- js/app.js
+++ js/app.js
@@ -1,129 +1,162 @@
-var dataTasks = []
+var dataTasks = [
+	'ноль дело'
+	,'раз дело'
+	,'2 дело'
+	,'3 дело'
+	,'4 дело'
+	,'5 дело'
+];
 
-var CheckTask = React.createClass({
-    render: function() {
+var dataTasks1 = [
+    'ноль дело измененное'
+    ,'раз дело измененное'
+    ,'2 дело измененное'
+    ,'3 дело измененное'
+    ,'4 дело измененное'
+    ,'5 дело измененное'
+];
+
+
+class Task extends React.Component {
+    render() {
+        var thisData = this.props.data;
+        var thisDivID = thisData.divID;
+        var thisCheckID = thisData.checkID;
+        var thisText = thisData.taskText;
         return (
-            <input type="checkbox" name="todolist"/>
+            <div className="task" id={thisDivID}>
+                <input type="checkbox" id={thisCheckID} />
+                <label> {thisText} </label>
+            </div>
         );
     }
-});
+}
 
-var LabelTask = React.createClass({
-    render: function() {
-        return (
-            <label></label>
-        );
-    }
-});
+class TaskList extends React.Component {
+/*    renderTask() {
+        //var thisData = this.props.data;
+        return <Task /> //<Task data={thisData} />
+    }*/
 
-var AllTasks = React.createClass({
-    render: function() {
+    render() {
+
         var data = this.props.data;
         var tasksTemplate = data.map(function(item, index) {
             var thisCheckID = "check" + {index}.index;
             var thisDivID = "div" + {index}.index;
+            var thisText = item;
+            var thisData = {taskText: thisText, divID: thisDivID, checkID: thisCheckID };
             return (
-                <div id={thisDivID} className="display">
-                    <input type="checkbox" name="todolist" id={thisCheckID}/>
-                    <label htmlFor={thisCheckID}>{item.taskText}</label>
-                </div>
+                <Task data={thisData} />
             )
         })
+
         return (
-            <div className="tasks">
+            <div className="TaskList">
                 {tasksTemplate}
             </div>
         );
     }
-});
+}
 
-var App = React.createClass({
-    render: function() {
+class Delete extends React.Component {
+    render() {
         return (
-            <div className="app">
-                <AllTasks data={dataTasks} />
+            <div className="Delete">
             </div>
         );
     }
-});
-
-ReactDOM.render(
-    <App />,
-    document.getElementById('list')
-);
-
-var Trash = React.createClass({
-    TrashClicked:  function() {
-        /*var checked = document.getElementById("check1").checked;
-        alert(checked);
-        dataTasks*/
-
-        for (var i = 0; i < dataTasks.length; i++)
-        {
-            var thisCheckID = "check" + i;
-            var thisDivID = "div" + i;
-            var checked = document.getElementById(thisCheckID).checked;
-            if (checked) document.getElementById(thisDivID).setAttribute("class", "display-none");
-        }
-    },
-    render: function() {
-        return (
-            <div>
-                TODO list
-                <div className="delete" onClick={this.TrashClicked}></div>
-            </div>
-        )
-    }
-});
-
-ReactDOM.render(
-    <Trash/>,
-    document.getElementById('header')
-);
-
-var Plus = React.createClass({
-    PlusClicked:  function() {
-        var valueTask = document.getElementById("newtask").value;
-        if (valueTask.replace(/\s/g, '').length < 3) alert("Слишком короткое задание!");
-        else
-        {
-            dataTasks[dataTasks.length] = {taskText: valueTask};
-            document.getElementById("newtask").value = "";
-            ReactDOM.render(
-                <App />,
-                document.getElementById('list')
-            );
-        }
-    },
-    render: function() {
-        return (
-            <div>
-                <div className="plus" onClick={this.PlusClicked}></div>
-                <input className="form-control" type="text" id="newtask" autoFocus="true"/>
-            </div>
-        )
-    }
-});
-
-ReactDOM.render(
-    <Plus/>,
-    document.getElementById('newTaskDiv')
-);
-
-document.onkeyup = function (e) {
-    e = e || window.event;
-    if (e.keyCode === 13) {
-        var valueTask = document.getElementById("newtask").value;
-        if (valueTask.replace(/\s/g, '').length < 3) alert("Слишком короткое задание!");
-        else
-        {
-            dataTasks[dataTasks.length] = {taskText: valueTask};
-            document.getElementById("newtask").value = "";
-            ReactDOM.render(
-                <App />,
-                document.getElementById('list')
-            );
-        }
-    }
-    return false;
 }
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div className="header">
+                <span class="h">
+                    TODO list
+                </span>
+                <Delete />
+            </div>
+        );
+    }
+}
+
+class New extends React.Component {
+
+    constructor() {
+        super();
+        this.state = {
+            newText: "  "
+        };
+    }
+
+    PlusClick(valueTask) {
+		//var valueTask = document.getElementById("newtaskinput").value;
+		alert(valueTask); /*TODO: delete this line*/
+        var thisData = this.props.data;
+        var thisOnClick = thisData.onClick;
+
+		if (valueTask.replace(/\s/g, '').length < 3) alert("Слишком короткое задание!");
+		else
+		{
+			thisOnClick(valueTask);
+			document.getElementById("newtaskinput").value = "";
+		}
+    }
+
+    OnChangeHandle(event) {
+        console.log('handleEmailChange', this); /*TODO delete this line*/
+        this.setState({newText: event.target.value});
+    }
+
+    OnSubmitHandle(event) {
+        event.preventDefault();
+        console.log('form submitted and email value is', this.state.newText);
+        alert('2form submitted and email value is' + this.state.newText);
+        this.PlusClick(this.state.newText);
+        return false;
+    }
+
+    render() {
+        return (
+            <form className="New"  onSubmit={this.OnSubmitHandle.bind(this)} >
+                <div className="Plus" onClick={() => this.PlusClick(this.state.newText) }></div>
+                <input className="form-control" type="text" id="newtaskinput" onChange={this.OnChangeHandle.bind(this)}/>
+            </form>
+        );
+    }
+}
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            tasks: dataTasks,
+        };
+    }
+
+    SetState(newText) {
+        var dataTasks1 = this.state.tasks;
+        dataTasks1[dataTasks1.length] = newText;
+        this.setState({
+            tasks: dataTasks1
+        });
+
+    }
+
+    render() {
+        var thisData = {onClick: this.SetState.bind(this)}
+        return (
+            <div class="App">
+                <Header/>
+                <TaskList data={this.state.tasks}/>
+                <New data={thisData} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(
+    <App/>,
+    document.getElementById('root')
+);
