4eea5f5b2e056d0f7353f1b03d8007f46f0d6948
annkupi <anna-kupina@yandex.ru>
2017-04-27 23:12:26 +0300

    -delete useless code

--- js/app.js
+++ js/app.js
@@ -1,11 +1,4 @@
-/*var dataTasks = [
-    {task: 'ноль дело', key: 1},
-	,'раз дело'
-	,'2 дело'
-	,'3 дело'
-	,'4 дело'
-	,'5 дело'
-];*/
+//Начальная инициализация
 
 var curKEY = 0;
 
@@ -24,18 +17,7 @@ dataTasks[dataTasks.length] = createTask('3 дело');
 dataTasks[dataTasks.length] = createTask('4 дело');
 dataTasks[dataTasks.length] = createTask('5 дело');
 
-
 var checkedItems = [];
-
-var dataTasks1 = [
-    'ноль дело измененное'
-    ,'раз дело измененное'
-    ,'2 дело измененное'
-    ,'3 дело измененное'
-    ,'4 дело измененное'
-    ,'5 дело измененное'
-];
-
 
 class Task extends React.Component {
 
@@ -45,19 +27,17 @@ class Task extends React.Component {
         var thisChangeHandle = thisData.onChange;
         var isChecked = event.target.checked;
 
-        //alert("mini checkbox" + thisIndex + "----" + isChecked);
-
         thisChangeHandle(thisIndex, isChecked);
     }
 
     render() {
         var thisData = this.props.data;
+
         var thisDivID = thisData.divID;
         var thisCheckID = thisData.checkID;
-        var thisText = thisData.taskText.tasktext; /*TODO look here for changes*/
+        var thisText = thisData.taskText.tasktext;
         var thisKey = thisData.taskText.key;
-        //var thisIndex = thisData.itemIndex;
-        //var thisChangeHandle = thisData.onChange;
+
         return (
             <div className="task" id={thisDivID}>
                 <input key={thisKey} type="checkbox" id={thisCheckID} onChange={this.ChangeCheckBoxHandle.bind(this)}/>
@@ -68,11 +48,6 @@ class Task extends React.Component {
 }
 
 class TaskList extends React.Component {
-/*    renderTask() {
-        //var thisData = this.props.data;
-        return <Task /> //<Task data={thisData} />
-    }*/
-
     render() {
 
         var data = this.props.data;
@@ -98,21 +73,9 @@ class TaskList extends React.Component {
     }
 }
 
-/*
-class Delete extends React.Component {
-    render() {
-        return (
-            <div className="Delete">
-            </div>
-        );
-    }
-}
-*/
-
 class Header extends React.Component {
 
     DeleteClick(){
-        //alert("delete!");
         var deleteHandle = this.props.onClick;
 
         deleteHandle();
@@ -146,8 +109,6 @@ class New extends React.Component {
     }
 
     PlusClick(valueTask) {
-		//var valueTask = document.getElementById("newtaskinput").value;
-		//alert(valueTask); /*TODO: delete this line*/
         var thisData = this.props.data;
         var thisOnClick = thisData.onClick;
 
@@ -161,14 +122,11 @@ class New extends React.Component {
     }
 
     OnChangeHandle(event) {
-        console.log('handleEmailChange', this); /*TODO delete this line*/
         this.setState({newText: event.target.value});
     }
 
     OnSubmitHandle(event) {
         event.preventDefault();
-        console.log('form submitted and email value is', this.state.newText);
-        //alert('2form submitted and email value is' + this.state.newText);
         this.PlusClick(this.state.newText);
         return false;
     }
@@ -203,47 +161,37 @@ class App extends React.Component {
 
     DeleteState() {
         var arr = this.state.checkedItems;
-        //alert("And delete now:" + " " + arr);
         var dataTasks1 = [];
 
         for (var i = 0; i < this.state.tasks.length; i++) {
             var curIndex = this.state.tasks[i];
-            //alert(i + "===> " + arr.indexOf(curIndex));
             if (arr.indexOf(i) == -1)
                 dataTasks1[dataTasks1.length] = curIndex;
         }
 
-        //alert("after delete datatasks1:" + " " + dataTasks1);
         arr =[];
 
         this.setState({
             tasks: dataTasks1,
             checkedItems: arr,
         });
-        //alert("after " + this.state.tasks);
     }
 
     SuperChangeCheckboxHandle(i, toAdd) {
         var checkedItems1 = this.state.checkedItems;
         if (toAdd) {
             checkedItems1[checkedItems1.length] = i;
-            //alert("Wow! super + change!" + i + toAdd + " " + checkedItems1);
         }
         else {
-            //alert("Wow! super - change!" + i + toAdd);
             var j = checkedItems1.indexOf(i);
-            //alert("indexof" + j);
             checkedItems1.splice(j, 1);
-            //alert("And now:" + " " + checkedItems1);
         }
 
         this.setState({checkedItems: checkedItems1});
-        //alert("And now:" + " " + this.state.checkedItems);
     }
 
     render() {
         var thisNewData = {onClick: this.SetState.bind(this)};
-        var thisDeleteData = this.DeleteState.bind(this);
         return (
             <div class="App">
                 <Header onClick={this.DeleteState.bind(this)} />
